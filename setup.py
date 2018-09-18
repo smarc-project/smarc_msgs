120aed4f085bff944e5f9e41bcc02c9301d3bd25
Nacho <torroba@kth.se>
2018-09-18 16:24:43 +0200

    Modified setup.py for scripts

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-setup_args = generate_distutils_setup(
-  scripts=[	'scripts/sm_task_utils.py']
-)
-
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['smarc_msgs'],
